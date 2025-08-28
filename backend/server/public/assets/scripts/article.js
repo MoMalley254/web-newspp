cf2eede14908760b89f480b3c215596829f3a9a7
Morris K.N <momalley042@gmail.com>
2025-08-28 12:53:59 +0300

    prepare to get images

--- backend/server/public/assets/scripts/article.js
+++ backend/server/public/assets/scripts/article.js
@@ -1,9 +1,39 @@
 const articlesLoader = document.querySelector(".loading-article-content");
 const dotsSpan = document.querySelector(".loader .dots");
+const flipContainer = document.getElementById('flipContainer');
 
 let dotCount = 0;
-
 const isMobile = detectDeviceType();
+
+document.addEventListener('DOMContentLoaded', async function() {
+    prepareFlipBook();  
+});
+
+function showErrorDiv(parentContainer, errorMessage) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div
+        class="alert alert-danger d-flex justify-content-between align-items-center"
+        role="alert"
+      >
+        <div>
+          <strong>Error:</strong> <br />
+          ${errorMessage}
+        </div>
+        <br />
+        <div>
+          <button
+            class="btn btn-outline-success btn-sm me-2"
+            onclick="location.reload()"
+          >
+            Reload
+          </button>
+        </div>
+      </div>
+    `;
+    parentContainer.innerHTML = div;
+}
+
 
 function detectDeviceType() {
   const ua = navigator.userAgent;
@@ -24,3 +54,13 @@ const dotsInterval = setInterval(() => {
   dotCount = (dotCount + 1) % 4;
   dotsSpan.textContent = ".".repeat(dotCount);
 }, 500);
+
+async function prepareFlipBook() {
+    try {
+        const resourceUrl = flipContainer.getAttribute('data-resource-url');
+        console.log(`Resource url ${resourceUrl}`);
+    } catch (error) {
+        console.error(`Prepare flipbook error ${error}`);
+        showErrorDiv(flipContainer, error);
+    }
+}
